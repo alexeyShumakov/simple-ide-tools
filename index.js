f6d1e8acf09e55bfe5b98f0678c50e4599678790
alexeyShumakov <bobshaftoe@yandex.ru>
2017-09-09 20:37:59 +0500

    comments

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+/*
+  Node v6.11.3
+  Используем все блага es6 так как нода 6 версии поддерживает
+  классные штуки из нового стандарта.
+
+  Оказалось все просто,
+  сначала "зипаем" название функций и даты,
+  затем сортируем по дате обновления.
+  Никакие алгоритмы сортировки придумывать не нужно, так как
+  стандартный сорт реализован "быстрой сортировкой",
+  а он достаточно эффективен с большими массивами!
+
+  Можете посмотреть мои тесты: https://github.com/alexeyShumakov/simple-ide-tools/blob/master/index.test.js
+*/
 let functionsArray = [];
 
 module.exports.refresh = (words, numbers) => {
@@ -6,6 +20,9 @@ module.exports.refresh = (words, numbers) => {
     .sort((a, b) => b.time - a.time);
 };
 
+/*
+  Изи.
+*/
 module.exports.guess = (str) => {
   return functionsArray
     .filter(el => el.name.substring(0, str.length) === str)
